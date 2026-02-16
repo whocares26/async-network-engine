c3230c8f80e956668c3957b65ae3f70ac196b6e7
whocares <whocares@debian.pc-vmware-debian>
2026-02-16 22:23:55 +0300

    UdpSocket class declared

---
+++ include/net/UdpSocket.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "net/Socket.hpp"
+#include "net/InetAddress.hpp"
+
+namespace net {
+
+class UdpSocket : public Socket {
+public:
+    UdpSocket();
+    explicit UdpSocket(int fd);
+
+    UdpSocket(const UdpSocket&) = delete;
+    UdpSocket& operator=(const UdpSocket&) = delete;
+
+    ssize_t sendto(const void* data, size_t length, const InetAddress& dest_addr);
+    ssize_t recvfrom(void* buffer, size_t length, InetAddress& src_addr);
+
+    void set_broadcast(bool on);
+};
+
+} // namespace net
