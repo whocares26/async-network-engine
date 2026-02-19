1dc78b75a3492635e280dd6f70c388f1312560ef
wowcircledeveloper <pooldiver21@mail.ru>
2026-02-19 19:49:10 +0300

    TcpClient: class declared

---
+++ include/net/TcpClient.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "net/EventLoop.hpp"
+#include "net/InetAddress.hpp"
+#include "net/TcpConnection.hpp"
+#include <functional>
+#include <memory>
+
+namespace net {
+
+class TcpClient {
+public: 
+    using ConnectionCallback = std::function<void(const std::shared_ptr<TcpConnection>&)>;
+    using ErrorCallback = std::function<void()>;
+
+    explicit TcpClient(EventLoop* loop);
+
+    ~TcpClient();
+
+    TcpClient(const TcpClient&) = delete;
+    TcpClient& operator=(const TcpClient&) = delete;
+
+    void connect(const InetAddress& serverAddr, ConnectionCallback onConnect, MessageCallback onMessage,
+                CloseCallback onClose, ErrorCallback onError = nullptr);
+
+    void cancel();
+
+private:
+    class Connector;
+    std::unique_ptr<Connector> m_connector;
+};
+
+} // namespace net
